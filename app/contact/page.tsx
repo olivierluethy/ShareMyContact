80e6a862aaa6b89e7ca6b74cc2cb78e0ece285be
BaskLash <olivier.luethy@gmx.net>
2026-05-12 10:15:03 +0200

    change it to dark theme

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 import { ContactForm } from "@easycontact/react"
 
 export default function ContactPage() {
-  return <ContactForm projectId="7ace45b807d94717481539fd" theme="light" />
+  return <ContactForm projectId="7ace45b807d94717481539fd" theme="dark" />
 }
